18eb906373674e8aa75c5ddb0579fcac2c5504e3
Kyle Macomber <kylemacomber@gmail.com>
2011-03-09 11:16:52 -0800

    Added OAuth constants

---
+++ Draftimo/Draftimo/DMConstants.h
@@ -0,0 +1,19 @@
+//
+//  DMConstants.h
+//  Draftimo
+//
+//  Created by Kyle Macomber on 3/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// OAuth
+extern NSString *const DMOAuthConsumerKey;
+extern NSString *const DMConsumerSecret;
+extern NSString *const YRequestTokenURL;
+extern NSString *const YUserAuthURL;
+
+// Fantasy API
+extern NSString *const YFBaseURL;
+extern NSString *const YFUserGamesMethod;
---
+++ Draftimo/Draftimo/DMConstants.m
@@ -0,0 +1,19 @@
+//
+//  DMConstants.m
+//  Draftimo
+//
+//  Created by Kyle Macomber on 3/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DMConstants.h"
+
+// OAuth
+NSString *const DMOAuthConsumerKey = @"dj0yJmk9QTJBaWhuTnozYXN4JmQ9WVdrOWNHaElObXhQTnpJbWNHbzlNVE13TVRZeE1UWTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD1mNg--";
+NSString *const DMConsumerSecret = @"a6cf76b76796d6afb558b604eaa1166e77cbfe6c";
+NSString *const YRequestTokenURL = @"https://api.login.yahoo.com/oauth/v2/get_request_token";
+NSString *const YUserAuthURL = @"https://api.login.yahoo.com/oauth/v2/request_auth";
+
+// Fantasy API
+NSString *const YFBaseURL = @"http://fantasysports.yahooapis.com/";
+NSString *const YFUserGamesMethod = @"/fantasy/v2/users;use_login=1/games";
