d866daba92253ea775b76abe976a2e677843ce6e
Kyle Macomber <kylemacomber@gmail.com>
2011-04-11 12:00:20 -0700

    Set 'Kyle Macomber' as Organization

--- Draftimo/Draftimo/DMDocumentWindowController.h
+++ Draftimo/Draftimo/DMDocumentWindowController.h
@@ -3,7 +3,7 @@
 //  Draftimo
 //
 //  Created by Kyle Macomber on 4/9/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Kyle Macomber. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
