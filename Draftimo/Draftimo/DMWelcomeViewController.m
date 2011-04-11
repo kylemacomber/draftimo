c45c98fd10f332eb751c1e1888a8092b85f98794
Kyle Macomber <kylemacomber@gmail.com>
2011-04-11 12:04:49 -0700

    Add WelcomeViewController
    The welcome view is show when the user creates a new draft before having
    linked Draftimo to their Yahoo! account.

---
+++ Draftimo/Draftimo/DMWelcomeViewController.m
@@ -0,0 +1,27 @@
+//
+//  DMWelcomeViewController.m
+//  Draftimo
+//
+//  Created by Kyle Macomber on 4/10/11.
+//  Copyright 2011 Kyle Macomber. All rights reserved.
+//
+
+#import "DMWelcomeViewController.h"
+
+
+@implementation DMWelcomeViewController
+
+- (id)init
+{
+    self = [super initWithNibName:NSStringFromClass([self class]) bundle:nil];
+    if (!self) return nil;
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
